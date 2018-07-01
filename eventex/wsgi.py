ad7ccb2594911e598f2efe81401df70c5403813c
ppedrinho10 <ppedrinho10@gmail.com>
2018-07-01 15:52:32 -0300

    Corrige wsgi.py

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-import dj_static __import Cling
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
